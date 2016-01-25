5c35b51e9a678fe6ebc9ef13f88505d7926c10f6
Syldarion <makelacaleb@gmail.com>
2016-01-24 19:51:23 -0800

    Updated gitignore and project settings

---
+++ PirateTBS/Assets/Scripts/DragObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject drag_object;
+    public Transform initial_parent;
+
+    void Start()
+    {
+
+	}
+
+	void Update()
+    {
+        	
+	}
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        drag_object = gameObject;
+        initial_parent = transform.parent;
+        transform.SetParent(GameObject.Find("Canvas").transform);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        drag_object = null;
+        if (transform.parent == initial_parent)
+            transform.SetParent(initial_parent, false);
+    }
+}
---
+++ PirateTBS/Assets/Scripts/DropObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class DropObject : MonoBehaviour, IDropHandler
+{
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+	}
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(transform.name);
+        eventData.pointerDrag.transform.SetParent(transform.GetChild(0));
+    }
+}
