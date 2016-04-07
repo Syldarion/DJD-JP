573c4bbe2e986594b57c8054937585930231080a
Caleb Makela <makelacaleb@gmail.com>
2016-04-07 14:02:04 -0700

    New tech tree init -- non-functional

---
+++ PirateTBS/Assets/Scripts/TechNode.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public delegate void OnActivateDelegate();
+
+public class TechNode : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public string Name;
+    public string EffectText;
+    public Image Icon;
+
+    public Color ActivatedColor;
+    public Color DeactivatedColor;
+
+    public bool IsActive;
+
+    public OnActivateDelegate OnActivation;
+
+	void Start()
+    {
+        if (Name != string.Empty)
+            TechTree.Instance.Nodes[Name] = this;
+        else
+            Destroy(gameObject);
+	}
+	
+	void Update()
+    {
+
+	}
+
+    public void ActivateNode()
+    {
+        if (IsActive)
+            return;
+
+        Icon.color = ActivatedColor;
+
+        IsActive = true;
+        OnActivation.Invoke();
+    }
+
+    public void DeactivateNode()
+    {
+        Icon.color = DeactivatedColor;
+
+        IsActive = false;
+    }
+
+    public void AddActivationAction(OnActivateDelegate action)
+    {
+        OnActivation += action;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!TechTree.Instance.ModifyingTree)
+            return;
+
+        if (IsActive)
+            DeactivateNode();
+        else
+            ActivateNode();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Tooltip.EnableTooltip(true);
+        Tooltip.UpdateTooltip(string.Format("{0}: {1}", Name, EffectText));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Tooltip.EnableTooltip(false);
+    }
+}
---
+++ PirateTBS/Assets/Scripts/TechTree.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TechTree : MonoBehaviour
+{
+    [HideInInspector]
+    public static TechTree Instance;
+
+    public Dictionary<string, TechNode> Nodes;
+
+    public bool ModifyingTree;
+
+	void Start()
+    {
+        Instance = this;
+        Nodes = new Dictionary<string, TechNode>();
+
+        ModifyingTree = false;
+	}
+	
+	void Update()
+    {
+
+	}
+}
